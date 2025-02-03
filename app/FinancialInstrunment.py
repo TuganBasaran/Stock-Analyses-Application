287ef2f6e1e063672e635e1466b9a139ca6425aa
Tugan_Basaran <64231122+TuganBasaran@users.noreply.github.com>
2025-02-03 21:23:30 +0300

    Initial commit of Stock Analysis Project

---
+++ app/FinancialInstrunment.py
@@ -0,0 +1,94 @@
+import pandas as pd 
+import numpy as np 
+import yfinance as yf
+import matplotlib.pyplot as plt 
+
+'''
+    Application for financial instrunment analysis 
+'''
+'''
+TODO:
+- Add error handling for API calls
+- Implement data validation
+- Add performance metrics calculations
+- Add portfolio optimization features
+'''
+
+class FinancialInstrunment(): 
+
+    def __init__(self, ticker, start, end):
+        '''Constructor for Financial Instrunment'''
+        self.ticker = ticker 
+        self.start = start 
+        self.end = end 
+        self.price_name = f"{self.ticker}_price"
+        self.log_return_name = f"{self.ticker}_log_return"
+        self.get_data()
+        self.log_returns()
+        
+
+    def __repr__(self):
+        return "Financial Instrunment (ticker: {}, start: {}, end: {})".format(self.ticker, self.start, self.end)
+    
+    def get_data(self): 
+        price_name = "{}_price".format(self.ticker)
+        raw = yf.download(tickers= self.ticker, start= self.start, end= self.end, multi_level_index=False).Close.to_frame()
+        raw.rename(columns={"Close" : price_name}, inplace=True)
+        self.data = raw 
+
+    def log_returns(self): 
+        self.data[self.log_return_name] = np.log(self.data / self.data.shift(1))
+
+    def _setup_plot(self, title): 
+        plt.grid(True)
+        plt.legend(fontsize=15)
+        plt.title(f"{self.ticker} {title}")
+        plt.show()
+
+    def plot_prices(self): 
+        price_name = "{}_price".format(self.ticker)
+        self.data[price_name].plot(figsize= (12,8))
+        self._setup_plot("Price Chart")
+    
+    def plot_returns(self, freq=None, kind='line'): 
+        """
+            Plot returns with specified frequency and kind (line or hist)
+            freqs = ["YE", "QE", "ME", "W-FRI", "D"]
+            periods = [1, 4, 12, 52, 252] for freqs 
+        """
+        if freq is None: 
+            self.data[self.log_return_name].plot(kind=kind)
+        else: 
+            resampled_price = self.data[self.price_name].resample(freq).last()
+            resampled_returns = np.log(resampled_price / resampled_price.shift(1))
+            resampled_returns.plot(kind = kind)
+        self._setup_plot("Log Return Chart")
+
+    def mean_return(self, freq="D"): 
+        '''
+        '''
+
+    
+    def std_mean_frame_ann(self): 
+        freqs = ["YE", "QE", "ME", "W-FRI", "D"]
+        periods = [1, 4, 12, 52, 252]
+        mean_list = []
+        std_list = []
+        for i in range(5): 
+            resampled_data = self.data[self.price_name].resample(freqs[i]).last()
+            resampled_return = np.log(resampled_data / resampled_data.shift(1))
+            mean = resampled_return.mean() * periods[i]
+            mean_list.append(mean)
+            std =  resampled_return.std() * np.sqrt(periods[i])
+            std_list.append(std)
+        frame = pd.DataFrame(data={"Std_Risk": std_list,"Mean_Reward": mean_list}, index=freqs)    
+        frame.plot(kind="scatter", x = "Std_Risk", y="Mean_Reward", figsize=(15,8), s=50, fontsize=15)
+        for i in frame.index: 
+            plt.annotate(i, xy=(frame.loc[i, "Std_Risk"] + 0.001, frame.loc[i, "Mean_Reward"] + 0.001), size = 15)
+        plt.ylim(0, 0.3)
+        plt.xlabel("Risk (Std)", fontsize = 15)
+        plt.ylabel("Return (Mean)", fontsize = 15)
+        plt.title("Annualized Risk & Return", fontsize = 20)
+        plt.show()
+
+            
