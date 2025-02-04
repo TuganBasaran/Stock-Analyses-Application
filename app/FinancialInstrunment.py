98a143882130504554ac7703b1f55f4fdd42dd6b
Tugan_Basaran <64231122+TuganBasaran@users.noreply.github.com>
2025-02-04 16:20:49 +0300

    Added a feature to plot_return() method, allowing to see Normal Distribution of Logarithmic Returns and created Instrunment Comparitor class to evaluate the difference of financial instrunments

--- app/FinancialInstrunment.py
+++ app/FinancialInstrunment.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import numpy as np 
 import yfinance as yf
+import scipy.stats as stats
 import matplotlib.pyplot as plt 
 
 '''
@@ -8,10 +9,11 @@ import matplotlib.pyplot as plt
 '''
 '''
 TODO:
-- Add error handling for API calls
-- Implement data validation
 - Add performance metrics calculations
-- Add portfolio optimization features
+- Add skewness calculation method 
+- Add kurtosisi calculation method 
+- Add rolling statistics (EMA 7/21/50 ....) 
+
 '''
 
 class FinancialInstrunment(): 
@@ -50,24 +52,46 @@ class FinancialInstrunment():
         self.data[price_name].plot(figsize= (12,8))
         self._setup_plot("Price Chart")
     
-    def plot_returns(self, freq=None, kind='line'): 
-        """
-            Plot returns with specified frequency and kind (line or hist)
-            freqs = ["YE", "QE", "ME", "W-FRI", "D"]
-            periods = [1, 4, 12, 52, 252] for freqs 
-        """
+    def mean_return(self, freq=None): 
         if freq is None: 
-            self.data[self.log_return_name].plot(kind=kind)
+            return self.data[self.log_return_name].mean()
         else: 
             resampled_price = self.data[self.price_name].resample(freq).last()
             resampled_returns = np.log(resampled_price / resampled_price.shift(1))
-            resampled_returns.plot(kind = kind)
-        self._setup_plot("Log Return Chart")
+        return resampled_returns.mean()
 
-    def mean_return(self, freq="D"): 
-        '''
-        '''
+    def std_risk(self, freq=None): 
+        if freq is None: 
+            return self.data[self.log_return_name].std()
+        else: 
+            resampled_price = self.data[self.price_name].resample(freq).last()
+            resampled_returns = np.log(resampled_price / resampled_price.shift(1))
+            return resampled_returns.std()
 
+    def plot_normal_distribution(self, returns): 
+        mu = returns.mean()
+        sigma = returns.std()
+        x = np.linspace(returns.min(), returns.max(), 10000)
+        y = stats.norm.pdf(x, loc=mu, scale=sigma)
+        plt.figure(figsize=(20, 8))
+        plt.hist(returns, bins=500, density=True, label=f"Frequency Distribution of Returns ({self.ticker})")
+        plt.plot(x, y, linewidth=3, color="red", label="Normal Distribution")
+        plt.title("Normal Distribution", fontsize=20)
+        plt.xlabel("Returns", fontsize=15)
+        plt.ylabel("pdf", fontsize=15)
+        plt.legend(fontsize=15)
+        plt.show()
+
+    def plot_returns(self, freq=None, kind="hist"): 
+        plt.figure(figsize=(12, 8))
+        if freq is None: 
+            returns = self.data[self.log_return_name]
+        else: 
+            resampled_price = self.data[self.price_name].resample(freq).last()
+            returns = np.log(resampled_price / resampled_price.shift(1))
+    
+        returns.plot(kind=kind)
+        self.plot_normal_distribution(returns)
     
     def std_mean_frame_ann(self): 
         freqs = ["YE", "QE", "ME", "W-FRI", "D"]
--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
 from FinancialInstrunment import FinancialInstrunment
 
 stock = FinancialInstrunment(ticker = "AAPL", start = "2015-01-01", end =  "2019-12-31")
-stock.std_mean_frame_ann()
+stock.plot_returns()
